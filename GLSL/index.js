29389487d772c2c451649ebceafd9ea81c271d70
Karmsetu <141593846+karmsetu@users.noreply.github.com>
2024-03-28 22:08:43 +0530

    shader#1 trignometric transformation on vertex shaders

--- GLSL/index.js
+++ GLSL/index.js
@@ -1,4 +1,6 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
     75,
@@ -7,6 +9,8 @@ const camera = new THREE.PerspectiveCamera(
     1000
 );
 const renderer = new THREE.WebGLRenderer();
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.zoomToCursor = true;
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
@@ -23,17 +27,35 @@ let loaded = false;
 // fragmentShader = `${fragmentShader}`;
 
 console.log(vertexShader, fragmentShader);
-const geometry = new THREE.PlaneGeometry(10, 10, 30, 30);
+camera.position.z = 5;
+controls.update();
+
+// shaders
+const uniforms = {
+    u_time: {
+        type: "f",
+        value: 0.0,
+    },
+};
+
+const geometry = new THREE.PlaneGeometry(5, 5, 30, 30);
 const material = new THREE.ShaderMaterial({
-    vertexShader: vertexShader,
-    fragmentShader: fragmentShader,
+    vertexShader,
+    fragmentShader,
     wireframe: true,
+    uniforms,
 });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
-camera.position.z = 5;
+
+const clock = new THREE.Clock();
+
 function animate() {
     requestAnimationFrame(animate);
+
+    // shader specific
+    uniforms.u_time.value = clock.getElapsedTime();
+    controls.update();
     if (!loaded) {
         if (vertexShader && fragmentShader) {
             loaded = true;
@@ -41,8 +63,8 @@ function animate() {
             console.log(fragmentShader);
         }
     }
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    // cube.rotation.x += 0.01;
+    // cube.rotation.y += 0.01;
     renderer.render(scene, camera);
 }
 
@@ -50,6 +72,7 @@ window.addEventListener("resize", (e) => {
     camera.aspect = window.innerWidth / window.innerHeight;
     renderer.setSize(window.innerWidth, window.innerHeight);
     camera.updateProjectionMatrix();
+    controls.update();
 });
 
 animate();
